be953b6f80a9674d1b50b8d2ea15589f8e738444
Alexander Standke <xanderstrike@gmail.com>
2015-08-07 18:22:18 -0700

    clean up and add some features

--- branch.rb
+++ branch.rb
@@ -6,8 +6,8 @@ include Curses
 
 init_screen
 start_color
-init_pair(COLOR_GREEN,COLOR_GREEN,COLOR_BLACK)
-init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK)
+init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK)
+init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK)
 
 GIT_BRANCH = `git branch`.split("\n")
 exit 0 if GIT_BRANCH.size == 0
@@ -28,25 +28,19 @@ ensure
   return input
 end
 
-def draw_cursor(draw)
-  attron(color_pair(COLOR_BLUE)|A_BOLD) {
-    addstr("#{ draw ? '> ' : '  ' }")
-  }
-end
-
 def draw_branches(selected)
   setpos(0, 0)
   addstr("Pick a branch\n")
 
   branches = GIT_BRANCH.each_with_index.map do |branch, i|
-    draw_cursor(i == selected)
+    attron(color_pair(COLOR_BLUE) | A_BOLD) do
+      addstr("#{ i == selected ? '> ' : '  ' }")
+    end
 
     branch_name = branch.gsub('  ', '')
     if branch_name.include?('* ')
       branch_name = branch_name.gsub('* ', '')
-      attron(color_pair(COLOR_GREEN)) {
-        addstr("#{branch_name}\n")
-      }
+      attron(color_pair(COLOR_GREEN)) { addstr("#{branch_name}\n") }
     else
       addstr("#{branch_name}\n")
     end
@@ -54,7 +48,7 @@ def draw_branches(selected)
     branch_name
   end
 
-  addstr("Use j/k and enter to select\n")
+  addstr("Use j/k and enter to select. Hit m for master.\n")
   refresh
   branches
 end
@@ -66,20 +60,24 @@ loop do
   c = read_char
 
   case c.downcase
-  when "\r"
+  when "\r", 'f'
     close_screen
     `git checkout #{branches[selected]}`
     exit 0
   when "\e[A", 'k'
-    selected -= 1 unless selected == 0
+    selected -= 1
   when "\e[B", 'j'
-    selected += 1 unless selected == branches.size - 1
+    selected += 1
+  when 'm'
+    close_screen
+    `git checkout master`
+    exit 0
   when "\e", "\u0003", 'q'
     close_screen
     exit 0
-  else
-    puts c.inspect
   end
+
+  selected = selected % branches.size
 
   draw_branches(selected)
 end
